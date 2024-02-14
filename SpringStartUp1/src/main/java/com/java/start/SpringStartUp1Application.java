ebe5723ed1a181fdef448f888d37e98c5c42bed6
anandmohansinha <anandsinhaknit@gmail.com>
2024-02-14 21:46:11 +0530

    initial commit

---
+++ SpringStartUp1/src/main/java/com/java/start/SpringStartUp1Application.java
@@ -0,0 +1,14 @@
+package com.java.start;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringStartUp1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringStartUp1Application.class, args);
+		
+	}
+
+}
---
+++ SpringStartUp1/src/test/java/com/java/start/SpringStartUp1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.start;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringStartUp1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
